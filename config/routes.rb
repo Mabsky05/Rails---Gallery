acec2202de1893acf6c4bc62dcd813bf1f0d602e
Mabsky05 <grimdango@gmail.com>
2025-06-09 01:27:13 +1000

    Added homepage, routes, TailwindCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get "home/index"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
