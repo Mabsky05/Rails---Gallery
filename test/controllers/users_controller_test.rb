668ee00e475ce71373f64a3ef6333526debac952
Mabsky05 <grimdango@gmail.com>
2025-06-10 22:02:26 +1000

    Working on user login/logout pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one) # or however you set up your test user
-  end
-
   test "should get show" do
-    # Log in the user
-    post session_path, params: { email_address: @user.user1@example.com, password: 'User1' }
-    get user_url(@user)
+    get users_show_url
     assert_response :success
   end
 end
