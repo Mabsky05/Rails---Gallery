4ea67aac9ac7948612ac3e90cfdaff5cc740d9b3
Mabsky05 <grimdango@gmail.com>
2025-06-12 23:02:30 +1000

    added unique user page upon authentication

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     # get users_show_url
+    post session_path, params: { email_address: "User1@example.com", password: "User1" }
     user = User.find_by(email_address: "User1@example.com")
     get users_show_url(user)
     assert_response :success
